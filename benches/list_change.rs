bf7312eb79a8bd8667a838a1e94cfed49f878e49
Quinn Pollock <contact@quinnpollock.net>
2020-03-25 16:34:50 -0400

    Add new benches

--- benches/list_change.rs
+++ benches/list_change.rs
@@ -1,48 +1,71 @@
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{
+    criterion_group, criterion_main, BatchSize, BenchmarkGroup, BenchmarkId, Criterion,
+};
 use criterion_cycles_per_byte::CyclesPerByte;
 use incremental_tree::choice::Choice;
 use incremental_tree::node::{Calculable, Node};
 use std::time::Duration;
 
-pub fn criterion_benchmark(c: &mut Criterion<CyclesPerByte>) {
-    let unmodfiedtree = Node::gen_node_of_depth(20);
-    let mut calced_tree = unmodfiedtree.clone();
-    calced_tree.calc();
-    c.bench_function("initial calculation", |b| {
-        b.iter_batched(
-            || unmodfiedtree.clone(),
-            |mut tree| tree.calc(),
-            BatchSize::LargeInput,
+pub fn benchmark_inital(c: &mut Criterion<CyclesPerByte>) {
+    let mut group: BenchmarkGroup<CyclesPerByte> = c.benchmark_group("non_nested_compare");
+
+    for i in 5..=20 {
+        let unmodfiedtree = Node::gen_node_of_depth(i);
+        let mut calced_tree = unmodfiedtree.clone();
+        calced_tree.calc();
+        group.bench_with_input(
+            BenchmarkId::new("initial calculation", i),
+            &unmodfiedtree,
+            |b, tree| {
+                b.iter_batched(
+                    || tree.clone(),
+                    |mut tree| tree.calc(),
+                    BatchSize::LargeInput,
+                );
+            },
         );
-    });
-    c.bench_function("modified from scratch", |b| {
-        b.iter_batched(
-            || {
-                let mut clean_tree = unmodfiedtree.clone();
-                clean_tree.define_modify(vec![Choice::Op, Choice::Right, Choice::Left], "+");
-                return clean_tree;
+        group.bench_with_input(
+            BenchmarkId::new("modified from scratch", i),
+            &unmodfiedtree,
+            |b, tree| {
+                b.iter_batched(
+                    || {
+                        let mut clean_tree = tree.clone();
+                        clean_tree
+                            .define_modify(vec![Choice::Op, Choice::Right, Choice::Left], "+");
+                        return clean_tree;
+                    },
+                    |mut clean_tree| clean_tree.calc(),
+                    BatchSize::LargeInput,
+                )
             },
-            |mut clean_tree| clean_tree.calc(),
-            BatchSize::LargeInput,
-        )
-    });
-    c.bench_function("incremental calc", |b| {
-        b.iter_batched(
-            || {
-                let inc_tree = calced_tree.clone();
-                return inc_tree;
+        );
+        group.bench_with_input(
+            BenchmarkId::new("incremental calc", i),
+            &calced_tree,
+            |b, tree| {
+                b.iter_batched(
+                    || {
+                        let inc_tree = tree.clone();
+                        return inc_tree;
+                    },
+                    |mut inc_tree| {
+                        inc_tree.define_modify_and_calc(
+                            vec![Choice::Op, Choice::Right, Choice::Left],
+                            "+",
+                        );
+                    },
+                    BatchSize::LargeInput,
+                )
             },
-            |mut inc_tree| {
-                inc_tree.define_modify_and_calc(vec![Choice::Op, Choice::Right, Choice::Left], "+");
-            },
-            BatchSize::LargeInput,
-        )
-    });
+        );
+    }
+    group.finish();
 }
 
 criterion_group!(
     name = benches;
     config = Criterion::default().with_measurement(CyclesPerByte).measurement_time(Duration::from_secs(30000));
-    targets =  criterion_benchmark
+    targets = benchmark_inital
 );
 criterion_main!(benches);
--- benches/tree_change.rs
+++ benches/tree_change.rs
@@ -1,8 +1,52 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{
+    criterion_group, criterion_main, BatchSize, BenchmarkGroup, BenchmarkId, Criterion,
+};
+use criterion_cycles_per_byte::CyclesPerByte;
+use incremental_tree::list::NodeList;
+use std::time::Duration;
 
-pub fn criterion_benchmark(_c: &mut Criterion) {
-    unimplemented!();
+pub fn benchmark_nested(c: &mut Criterion<CyclesPerByte>) {
+    let mut group: BenchmarkGroup<CyclesPerByte> = c.benchmark_group("nested_compare");
+
+    for i in [100, 300, 600, 900, 1200, 1600, 2000].iter() {
+        for j in 5..=20 {
+            let parameter_string = format!("list of {} nodes with depth of {}", i, j);
+            let n_list = NodeList::gen_random_of_depth(*i, j);
+            let mut calced_and_sorted_list = n_list.clone();
+            calced_and_sorted_list.sort();
+            group.bench_with_input(
+                BenchmarkId::new("from_scatch", &parameter_string),
+                &n_list,
+                |b, node_list| {
+                    b.iter_batched(
+                        || node_list.clone(),
+                        |mut list| {
+                            list.sort();
+                        },
+                        BatchSize::LargeInput,
+                    );
+                },
+            );
+            group.bench_with_input(
+                BenchmarkId::new("incremental sort", &parameter_string),
+                &calced_and_sorted_list,
+                |b, cs_list| {
+                    b.iter_batched(
+                        || cs_list.clone(),
+                        |mut list| {
+                            list.modify_first_element();
+                        },
+                        BatchSize::LargeInput,
+                    )
+                },
+            );
+        }
+    }
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().with_measurement(CyclesPerByte).measurement_time(Duration::from_secs(30000));
+    targets =  benchmark_nested
+);
 criterion_main!(benches);
